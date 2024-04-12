b19359126d25386cc8e68c42f4ffd95e2200d33a
leeminseop <lms990108@gmail.com>
2024-04-12 22:24:55 +0900

    Chore: 초기설정 진행중

---
+++ main.py
@@ -0,0 +1,11 @@
+# 실행 명령어 uvicorn main:app --reload
+# 포트번호 : 8000번
+# api list : localhost:8000/docs
+from fastapi import FastAPI
+from app.router import testRouter
+
+app = FastAPI()
+
+app.include_router(testRouter, prefix="/api/test")
+
+
