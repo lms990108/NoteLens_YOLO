1669e7dd27892ca071ed1ff4d210287bcec87835
leeminseop <lms990108@gmail.com>
2024-04-26 21:40:37 +0900

    Feat: 이미지라우터 초안 작성, 불필요한 테스트라우터는 제거

--- main.py
+++ main.py
@@ -2,10 +2,8 @@
 # 포트번호 : 8000번
 # api list : localhost:8000/docs
 from fastapi import FastAPI
-from app.router import testRouter
+from app.router import testRouter, imageRouter
 
 app = FastAPI()
 
-app.include_router(testRouter, prefix="/api/test")
-
-
+app.include_router(imageRouter, prefix="/api/image")
