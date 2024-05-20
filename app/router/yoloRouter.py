62123da4a92a5379a416fed8363ff81fe82cd1d8
Ju Yeongun <cutr1005@naver.com>
2024-05-20 23:32:50 +0900

    Feat: ocr 서버와 통신 구현(아직 파일 하나로만 통신 구현함)

--- app/router/yoloRouter.py
+++ app/router/yoloRouter.py
@@ -4,12 +4,18 @@ from typing import List
 import os
 from io import BytesIO
 
+
+
+from pathlib import Path
+import httpx
 import logging
 from app.service.yoloService import YOLOv5Service
 
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 
-
-
+# 로그 설정
 logging.config.fileConfig('app/config/logging_config.ini')
 logger = logging.getLogger(__name__)
 
@@ -18,9 +24,11 @@ weights_path = 'yolov5/weights/best.pt'
 yolov5_service = YOLOv5Service()
 logger.info(f"YOLOv5 가중치 경로: {weights_path}")
 
-
 yoloRouter = APIRouter()
 
+
+# api 정의
+# 이미지를 파일로 직접 받아서 YOLOv5로 객체 탐지 수행
 @yoloRouter.post("/yolo", response_model=List[str])
 async def process_image(file: UploadFile = File(...)):
     # 임시 저장할 파일 경로
@@ -29,19 +37,57 @@ async def process_image(file: UploadFile = File(...)):
         buffer.write(await file.read())
     logger.info(f"임시 파일 경로: {temp_file_path}")
     
-    # yolo로 이미지 크롭 실행
-    rst = yolov5_service.test_detect(temp_file_path)
+    # yolo로 이미지 크롭, ocr 수행 후 결과 리턴
+    # yolov5_service.test_textDetectionAndOCR(temp_file_path)
     logger.info("YOLOv5 /yolo FILE 객체 탐지를 성공적으로 수행했습니다.")
     
     # 임시 파일 삭제
-    # os.remove(temp_file_path)
-    # logger.info("임시 파일을 성공적으로 삭제했습니다.")
+    os.remove(temp_file_path)
+    logger.info("임시 파일을 성공적으로 삭제했습니다.")
     
-    return rst
-
+    # 몽고아이디로 파일에 접근
+    mongo_id = "TEST_mongo_id"
+    dir_path = Path("yolov5") / "runs" / "detect" / mongo_id / "crops" / "underline text"
+    
+    # 크롭된 이미지들을 files로 저장 TODO 구현 필요
+    
+    
+    
+    # 크롭된 이미지의 경로 설정
+    file_path = dir_path / f"{mongo_id}.jpg"
+    if not Path(file_path).is_file():
+        raise FileNotFoundError(f"File not found: {dir_path / '{mongo_id}.jpg'}")
     
 
+    # 크롭된 이미지들을 ocr 서비스로 전달
+    url = "http://43.203.54.176:8000/api/ocr/ocr" # ocr 서비스 주소
+    async with httpx.AsyncClient() as client:
 
+        try:
+            with open(file_path, "rb") as f:
+                files_data = {
+                    'file': (Path(file_path).name, f, 'image/jpeg')
+                }
+                logger.info("httpx 작업 중 - files_data: ", files_data)
+                response = await client.post(url, files=files_data)
+            
+            logger.info("httpx 작업 중 - await 이후")
+            response.raise_for_status()  # 응답 상태 코드가 4xx 또는 5xx인 경우 예외 발생
+            result_texts = response.json()
+            logger.info("httpx 작업 수행됨 올바른지 아닌지 모름")
+            return result_texts
+        except httpx.RequestError as exc:
+            # 요청 중에 발생한 네트워크 오류 처리
+            raise HTTPException(status_code=500, detail=f"Error while requesting server2: {exc}")
+        except httpx.HTTPStatusError as exc:
+            # 서버2에서 반환한 오류 상태 코드 처리
+            raise HTTPException(status_code=exc.response.status_code, detail=f"Error response from server2: {exc.response.text}")
+        except Exception as exc:
+            # 기타 예외 처리
+            raise HTTPException(status_code=500, detail=f"An unexpected error occurred: {exc}")
+
+
+# url로 이미지를 받아서 YOLOv5로 객체 탐지 수행
 @yoloRouter.post("/yolo-from-url", response_model=List[str])
 async def process_image_from_url(image_url: str):
     try:
@@ -50,16 +96,15 @@ async def process_image_from_url(image_url: str):
     except requests.RequestException as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-    # image_bytes = BytesIO(response.content)
-    image_path = f"temp_{image_url.split('/')[-1]}"
+    image_bytes = BytesIO(response.content)
+    temp_image_path = f"temp_{image_url.split('/')[-1]}"
     
-    # with open(image_path, 'wb') as image_file:
-    #     image_file.write(image_bytes.read())
+    with open(temp_image_path, 'wb') as image_file:
+        image_file.write(image_bytes.read())
 
-    # texts = ocr_service.perform_ocr(image_path)
+    result_texts = yolov5_service.test_textDetectionAndOCR(temp_image_path)
     
-    # os.remove(image_path)
+    os.remove(temp_image_path)
     
-    # return texts
+    # return result_texts
     
-    yolov5_service.detect_objects(image_path)
--- app/service/yoloService.py
+++ app/service/yoloService.py
@@ -3,6 +3,7 @@ import logging
 from PIL import Image
 import io
 
+
 from yolov5 import test_detection
 from torchvision.transforms import functional as F
 
@@ -14,41 +15,46 @@ logger.info("YOLOv5Service 클래스 정의")
 
 class YOLOv5Service:
     def __init__(self):
-        # try:
-        #     if weights_path:
-        #         # 사용자 지정 가중치 파일을 사용하여 모델 로드
-        #         # self.model = torch.hub.load('/yolov5', 'custom', path=weights_path)
-        #         # 로컬 레포의 yolov5, 커스텀 가중치 파일로 모델 로드
-        #         self.model = torch.hub.load('yolov5', 'custom', source='local', path=weights_path)
-        #         logger.info(f"YOLOv5 모델이 {weights_path}에서 가중치를 성공적으로 로드했습니다.")
-        #     else:
-        #         # 기본 사전 학습된 yolov5s 모델 로드
-        #         self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-        #         logger.info("YOLOv5 사전 학습된 모델이 성공적으로 로드되었습니다.")
-        # except Exception as e:
-        #     logger.error(f"YOLOv5 모델 로드 중 오류 발생: {e}")
-        #     raise
-        
         self.test = test_detection.run
     
-    def test_detect(self, image_path):
+    def test_textDetectionAndOCR(self, image_path):
         logger.info("test_detect 함수 실행")
-        # 객체 탐지 수행
-
-        results = self.test(source=image_path)
+        
+        # 크롭된 이미지들이 경로에 저장됨
+        self.test(source=image_path)
         logger.info("test_detect 내 test_detection 함수를 성공적으로 수행했습니다.")
         
-        # 탐지된 객체의 위치 정보를 사용하여 이미지를 잘라냅니다.
-        cropped_images = []
-        for result in results:
-            # 객체의 좌표를 가져와서 이미지를 잘라냅니다.
-            x1, y1, x2, y2 = result['bbox']
-            cropped_img = image_path.crop((x1, y1, x2, y2))
-            cropped_images.append(cropped_img)
-
-        logger.info("크롭 이미지 리턴")
-        return cropped_images
         
+        
+        
+        # ocr 서비스로부터 결과를 받아서 리턴
+        
+        
+        
+        # logger.info("이미지의 텍스트 탐지 및 OCR의 결과 리턴")
+        # return result_texts
+
+
+
+    # mongo_id로 접근하는 것이 최종 목표
+    # def textDetectionAndOCR(self, image_path, mongo_id): 
+        # logger.info("test_detect 함수 실행")
+        
+        # # 크롭된 이미지들이 경로에 저장됨
+        # self.test(source=image_path, mongo_id=mongo_id)
+        # logger.info("test_detect 내 test_detection 함수를 성공적으로 수행했습니다.")
+        
+        # # 크롭된 이미지들을 ocr 서비스로 전달
+        
+        
+        # # ocr 서비스로부터 결과를 받아서 리턴
+        
+        
+        
+        # logger.info("이미지의 텍스트 탐지 및 OCR의 결과 리턴")
+        # return result_texts
+    
+    
 
     def detect_objects(self, image_path):
         
